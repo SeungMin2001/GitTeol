1ee8c1b2f19dea72c0ad6e7f0d57fb979026cac3
SeungMin <dolkuji1@naver.com>
2025-10-02 13:54:54 +0900

    add message

--- app/home/search.js
+++ app/home/search.js
@@ -4,6 +4,9 @@ const search=()=>{
     return(
         <View>
             <Text>search</Text>
+            <Text>
+                레포지토리 안에 이미지 활용. 격자들에 크기를 어떻게 다르게 할까 고민해야함.
+            </Text>
         </View>
     )
 }
